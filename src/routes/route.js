2f7e285130ff786ee3cefb452c7126de19b6ff5d
prasoongupta001 <prasoongupta351@gmail.com>
2022-02-24 22:08:12 +0530

    solution of wednesday

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,13 +7,45 @@ router.post("/players",function(req,res){
     let playerName = player.name
     for (let i=0; i<players.length; i++){
         if(players[i].name==playerName){
-            res.send('player already exists')
+           return res.send('player already exists')
         }
 
     }
     players.push(player);
     console.log("here is the player from request", players)
     res.send(players)
+});
+
+router.post('/players/:playerName/bookings/:bookingId',function(req,res){
+    let name = req.params.playerName
+    let isPlayerPresent= false
+    for(let i=0; i<players.length; i++){
+        if(players[i].name==name){
+            isPlayerPresent =true
+        }
+    }
+
+    if(!isPlayerPresent){
+       return res.send('Player Not Present')
+    }
+    
+    let booking=req.body
+    let bookingId=req.params.bookingId
+    for(let i=0; i<playerName; i++){
+        if (players[i].name == name){
+            let isbookingPresent =false
+            for(let j=0; j<player[i].bookings.length; j++){
+                if(players[i].bookings[j].bookingNumber== bookingId){
+                    return res.send('booking with this id is already exists for the player')
+
+                }
+            }
+            players[i].bookings.push(booking)
+        }
+    }
+
+    res.send(players)
+
 });
 
 
@@ -96,6 +128,48 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+//take marks in a req.query variable named"marks" and send "pass ", if >40 else "fail"
+
+router.get("/get-query-2",function(req,res){
+    let marks= req.query.marks
+    let result 
+    if(marks>40){
+        result = "pass"
+    }else{
+        result="fail"
+    }
+
+    res.send({ result : result , status:true})
+})
+
+
+let myarr=[23,32,45,6,85,98,55,25,46,74,98]
+router.post("/postquery5", function(req,res){
+    let input=req.query.input
+    let finalarr=""
+    for(let i=0; i<myarr.length; i++){
+    if(myarr[i]> input){ finalarr+= " " +myarr[i]}
+    }
+    res.send({ result : finalarr , status:true})
+
+})
+
+
+
+
+
+router.get("/get-query-1", function(req,res){
+    let data = req.query
+    console.log(data)
+    res.send({ data : data , status:true})
+})
+
+
+
+
+
+
 
 
 module.exports = router;
